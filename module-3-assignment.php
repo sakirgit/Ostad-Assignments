c3bd13debd953091ecb79aff5231b4ab0145f541
Sakir Ahamed <sakiremail@gmail.com>
2023-02-13 00:33:45 +0600

    module-3-assignment

---
+++ module-3-assignment.php
@@ -0,0 +1,27 @@
+
+/* ====================================
+1. Write a Reusable  PHP Function that can check Even & Odd Number And Return Decision
+==================================== */
+
+function check_even_odd($number) {
+    if ($number % 2 == 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+
+$num = 5;
+$result = check_even_odd($num);
+echo "$num is $result";
+ 
+
+/* ====================================
+2. 1+2+3...…….100  Write a loop to calculate the summation of the series
+==================================== */
+
+$sum = 0;
+for ($i = 1; $i <= 100; $i++) {
+    $sum += $i;
+}
+echo "The summation of the series 1 + 2 + 3 + ... + 100 is $sum";
